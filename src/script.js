1839ce600a05e3babdcfc1b1b9dea5dd16ab14c2
joyene <joymenyene@gmail.com>
2023-07-20 14:06:18 +0100

    images completed

---
+++ src/script.js
@@ -0,0 +1,4 @@
+document.getElementById("food").innerHTML = "Welcome to my food gallery";
+document.getElementById("soup-gallery").innerHTML = "Soups";
+document.getElementById("pastries").innerHTML = "Pastries";
+document.getElementById("other-foods").innerHTML = "Other foods";
